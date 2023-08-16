da573abf489f8a74104df8e676177cdf7ea91449
ChristianTC <christiantambo15@gmail.com>
2023-08-16 00:26:08 -0400

    fetch 100 rows of data using the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import axios from "axios"
+
+import './App.css'
+import { useState, useEffect } from 'react';
+import { IUser } from "./interfaces/interfaces";
+
 const App = () => {
 
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?results=100')
+      .then(response => setUsers(response.data.results))
+      .catch(err => console.error(err))
+  }, [])
+  
+
   return (
-    <>
-      APP
-    </>
+    <div className="app">
+      <h1>USERS APP</h1>
+      { JSON.stringify(users) }
+    </div>
   )
 }
 
